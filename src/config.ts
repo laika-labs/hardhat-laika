84088295f56f16bd077a8441c3c5ae2ad9a1287e
Nattapon Sukthaworn <nattapon33998@gmail.com>
2022-03-24 12:43:32 +0700

    chore: endpoint v2

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ const endpointUrls: {
   [name: string]: string;
 } = {
   interface: "https://web.getlaika.app",
-  services: "https://api.getlaika.app",
+  services: "https://api.getlaika.app/v2",
 };
 
 export { endpointUrls };
