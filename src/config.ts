a313204d1e3d55a80a631d38503f0981e7baaaeb
Nithi Nomprawat <nithi.boss157@gmail.com>
2022-08-19 21:14:22 +0700

    feat: add interface pro endpoint

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ const endpointUrls: {
   [name: string]: string;
 } = {
   interface: "https://web.getlaika.app",
+  interface_pro: "https://pro.getlaika.app",
   services: "https://api.getlaika.app",
 };
 
